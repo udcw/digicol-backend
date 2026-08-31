bee3d706e9bf82afc9cd0568c8c35d6b8949b5ce
udcw <udemycodewarrios@gmail.com.com>
2026-08-31 22:19:44 +0100

    Fix: Courses admin ultra-simple

--- courses/admin.py
+++ courses/admin.py
@@ -1,16 +1,1 @@
-# courses/admin.py
-
 from django.contrib import admin
-from .models import Category, Course, Enrollment
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'slug']
-
-@admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'category', 'level', 'price', 'is_published']
-
-@admin.register(Enrollment)
-class EnrollmentAdmin(admin.ModelAdmin):
-    list_display = ['id', 'member', 'course', 'status']
