680566ec0fdb2635f066cce73684399c9727f3a7
udcw <udemycodewarrios@gmail.com.com>
2026-08-31 18:17:39 +0100

    Fix: Admin vidé temporairement

--- courses/admin.py
+++ courses/admin.py
@@ -1,17 +1,1 @@
 from django.contrib import admin
-from .models import Category, Course, Enrollment
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-
-@admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'level', 'price', 'is_published']
-    list_filter = ['category', 'level', 'is_published']
-    search_fields = ['title', 'description']
-
-@admin.register(Enrollment)
-class EnrollmentAdmin(admin.ModelAdmin):
-    list_display = ['member', 'course', 'status', 'enrollment_date']
-    list_filter = ['status']
